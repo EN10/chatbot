738ffac7b66e10a54393b04716ffad99fabf26ba
enio <ict@cic.ac>
2017-01-16 18:57:59 +0000

    training time

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorf
 sudo pip install --upgrade $TF_BINARY_URL
 
 #git clone -b latest --single-branch --depth 1 https://github.com/EN10/chatbot.git
+
+#training time estimate:
+#3 mins Prepare data
+#~ 20 mins for checkpoint
