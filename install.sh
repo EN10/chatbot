32f064a21d9fa96140b1b02e098d218ab405e7b6
enio <ict@cic.ac>
2017-01-17 20:00:34 +0000

    docker

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ pip install numpy scipy six tensorflow
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.1-cp27-none-linux_x86_64.whl
 sudo pip install --upgrade $TF_BINARY_URL
 
+#docker run -it gcr.io/tensorflow/tensorflow:latest-devel
 #git clone -b latest --single-branch --depth 1 https://github.com/EN10/chatbot.git
 
 #training time estimate:
